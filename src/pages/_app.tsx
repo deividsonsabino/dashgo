75c06eae9ea17671d320ab806c301878c95a5208
deivao <xdeividsabinox@live.com>
2022-02-28 17:27:56 -0300

    feat: configurando chackra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps } : AppProps) {
-  return <Component {...pageProps} />
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../styles/theme'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
