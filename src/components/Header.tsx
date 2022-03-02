2042e50b29830b32a3dc6515eaff5e3e3f6b5bba
deivao <xdeividsabinox@live.com>
2022-03-02 20:12:29 -0300

    feat: finalizando o header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react'
-import { RiSearchLine} from 'react-icons/ri'
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 export function Header() {
     return (
         <Flex
@@ -39,9 +39,40 @@ export function Header() {
                     px="4"
                     mr="4"
                     placeholder="Buscar na plataforma"
-                    _placeholder={{ color: 'gray.400'}}
+                    _placeholder={{ color: 'gray.400' }}
                 />
-                <Icon as={RiSearchLine} fontSize="20"/>
+                <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+
+            <Flex
+                align="center"
+                ml="auto"
+            >
+                <HStack
+                    spacing="8"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+
+                <Flex align="center">
+                    <Box mr="4" textAlign="right">
+                        <Text>Deividson Sabino</Text>
+                        <Text color="gray.300" fontSize="small">deividsonsabino@outlook.com.br</Text>
+                    </Box>
+                    <Avatar
+                        size="md"
+                        name="Deividson Sabino"
+                        src="https://github.com/deivaotv.png"
+                    />
+                </Flex>
+
             </Flex>
         </Flex>
     );
