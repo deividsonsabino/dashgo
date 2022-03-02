d382554d967b6c257a94afcc05091111197f8d38
deivao <xdeividsabinox@live.com>
2022-03-02 19:14:21 -0300

    feat: página de login

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,12 @@
-import Document, { Html, Main,Head, NextScript } from 'next/document';
+import Document, { Html, Main, NextScript } from 'next/document';
+import Head from 'next/head';
 
 export default class MyDocument extends Document {
     render() {
         return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
