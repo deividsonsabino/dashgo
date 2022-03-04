9f61c9540e0cc9f99ba4ae20fd1eb6b7f3d174ff
deivao <xdeividsabinox@live.com>
2022-03-03 23:20:38 -0300

    feat: header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,26 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align="center">
-        <Box mr="4" textAlign="right">
-            <Text>Deividson Sabino</Text>
-            <Text color="gray.300" fontSize="small">deividsonsabino@outlook.com.br</Text>
-        </Box>
-        <Avatar
-            size="md"
-            name="Deividson Sabino"
-            src="https://github.com/deividsonsabino.png"
-        />
-    </Flex>
+            {showProfileData &&
+                (
+                    <Box mr="4" textAlign="right">
+                        <Text>Deividson Sabino</Text>
+                        <Text color="gray.300" fontSize="small">deividsonsabino@outlook.com.br</Text>
+                    </Box>
+                )
+            }
+
+            <Avatar
+                size="md"
+                name="Deividson Sabino"
+                src="https://github.com/deividsonsabino.png"
+            />
+        </Flex>
     )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, useBreakpointValue } from '@chakra-ui/react'
 
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 export function Header() {
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg:true
+    })
     return (
         <Flex
             as="header"
@@ -17,13 +21,13 @@ export function Header() {
             align="center"
         >
             <Logo/>
-            <SearchBox />
+            {isWideVersion && <SearchBox />}
             <Flex
                 align="center"
                 ml="auto"
             >
                 <NotificationsNav />
-                <Profile />
+                <Profile showProfileData={isWideVersion} />
             </Flex>
         </Flex>
     );
