e44a8dfd273ece277b0945fd7388aff97a4b104c
deivao <xdeividsabinox@live.com>
2022-03-04 00:06:28 -0300

    feat: listagem responsiva

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -11,7 +11,8 @@ import {
     Th,
     Thead,
     Tr,
-    Text
+    Text,
+    useBreakpointValue
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
@@ -19,6 +20,10 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true
+    })
     return (
         <Box>
             <Header />
@@ -55,17 +60,17 @@ export default function UserList() {
                     >
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" w="8">
+                                <Th px={["4", "4", "6"]} color="gray.300" w="8">
                                     <Checkbox colorScheme="pink" />
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
+                                {isWideVersion && (<Th>Data de cadastro</Th>)}
                                 <Th w="8"></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4", "4", "6"]}>
                                     <Checkbox colorScheme="pink" />
                                 </Td>
                                 <Td>
@@ -74,19 +79,21 @@ export default function UserList() {
                                         <Text fontSize="sm" color="gray.300">deividsonsabino@outlook.com.br</Text>
                                     </Box>
                                 </Td>
-                                <Td>
+                                {isWideVersion && (<Td>
                                     04 de março, 2022
-                                </Td>
+                                </Td>)}
                                 <Td>
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                    >
-                                        Editar
-                                    </Button>
+                                    {isWideVersion && (
+                                        <Button
+                                            as="a"
+                                            size="sm"
+                                            fontSize="sm"
+                                            colorScheme="purple"
+                                            leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                        >
+                                            Editar
+                                        </Button>
+                                    )}
                                 </Td>
                             </Tr>
                         </Tbody>
