95418956d0439d10e6eef4c15b93d1b351c620ca
deivao <xdeividsabinox@live.com>
2022-03-04 00:20:07 -0300

    feat: fluxo de navegação

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,17 +1,20 @@
-import { Icon, Link, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+import { Icon, Link as ChakraLink, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
 import { ElementType } from "react";
-import { RiInputMethodLine } from "react-icons/ri";
-
+import Link from 'next/link'
 interface NavLinkProps extends ChakraLinkProps {
     icon: ElementType;
     children: string;
+    href: string;
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
     return (
-        <Link display="flex" {...rest}>
-            <Icon as={icon} fontSize="20" />
-            <Text ml="4" fontWeight="medium">{children}</Text>
+        <Link href={href} passHref>
+            <ChakraLink display="flex" {...rest}>
+                <Icon as={icon} fontSize="20" />
+                <Text ml="4" fontWeight="medium">{children}</Text>
+            </ChakraLink>
         </Link>
+
     )
 }
