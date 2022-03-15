a6d23b8f0c8e181878a645f722d64850460667d5
deividsonsabino <deividsonsabino@outlook.com.br>
2022-03-14 23:43:50 -0300

    feat: formulários no React

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,7 +1,13 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
+import { useRef } from "react";
 import { RiSearchLine } from "react-icons/ri";
 
 export function SearchBox() {
+
+    const searchInputRef = useRef<HTMLInputElement>(null)
+
+    console.log(searchInputRef.current.value);
+    
     return (
         <Flex
             as="label"
@@ -23,6 +29,7 @@ export function SearchBox() {
                 mr="4"
                 placeholder="Buscar na plataforma"
                 _placeholder={{ color: 'gray.400' }}
+                ref={searchInputRef}
             />
             <Icon as={RiSearchLine} fontSize="20" />
         </Flex>
