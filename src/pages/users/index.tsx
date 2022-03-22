fce957a138399fe909ab3743656ccdbdd190ab73
deividsonsabino <deividsonsabino@outlook.com.br>
2022-03-22 19:29:57 -0300

    feat: criando hook de listagem de usuário

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,34 +17,14 @@ import {
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from 'react-query'
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-    const { data, isLoading, error, isFetching } = useQuery('users', async () => {
-        const { data } = await api.get('/users')
-
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString('pt-Br', {
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric'
-                }),
-            }
-        })
-
-        return users
-    }, {
-        staleTime: 1000 * 5
-    })
+    const { data, isLoading, error, isFetching } = useUsers()
 
     const isWideVersion = useBreakpointValue({
         base: false,
